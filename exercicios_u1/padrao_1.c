4d5e8d85d3280c833760bfda6c07d7c39b780985
jotavlim4 <jotav.lim4@gmail.com>
2023-09-04 04:15:11 -0300

    Inclusão de comentários na solução do exercício 8

--- exercicios_u1/padrao_1.c
+++ exercicios_u1/padrao_1.c
@@ -6,12 +6,16 @@ int main()
 
     scanf("%d", &n);
 
+    //escreve as n colunas
     for (int i = 0; i < n; i++)
     {
+        //inicia a linha i+1, pois i = 0 e vai até n + i
+        //para poder completar a linha com 4 números consecutivos
         for (int j = i + 1; j <= n + i; j++)
         {
             printf("%d ", j);           
         }
+        //imprime o caractere new line
         putchar('\n');
         
     }
