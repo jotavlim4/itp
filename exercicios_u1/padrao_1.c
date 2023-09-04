75093b02011c629394ca896c4e623677816c44f9
jotavlim4 <jotav.lim4@gmail.com>
2023-09-04 04:11:40 -0300

    Solução exercício 8, lista laços, conclusão da lista de exercícios.

---
+++ exercicios_u1/padrao_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j <= n + i; j++)
+        {
+            printf("%d ", j);           
+        }
+        putchar('\n');
+        
+    }
+    return 0;
+}
