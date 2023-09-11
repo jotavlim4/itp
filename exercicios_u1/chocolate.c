b7130522e498986f92769c6a42fb436f38993e0d
jotavlim4 <jotav.lim4@gmail.com>
2023-09-11 15:22:01 -0300

    Atualização exercício

--- exercicios_u1/chocolate.c
+++ exercicios_u1/chocolate.c
@@ -10,20 +10,30 @@ int main()
     scanf("%d %d", &l, &h);
 
     count = 0;
+    //fluxo para o caso do "primeiro lado" ser o maior
     if (l > h)
     {
+        //executa até que os lados sejam iguais
         while (l != h)
         {
+            //divide o valor do lado maior por 2
+            //conta a ocorrencia da divisao
+            //atualiza o tamanho do maior lado
             new_size = l / 2;
             count++;
             l = new_size;
         }
         
     }
+    //fluxo para o caso do "segundo lado" ser o maior
     else
     {
+        //executa até que os lados sejam iguais
         while (l != h)
-        {
+        {   
+            //divide o valor do lado maior por 2
+            //conta a ocorrencia da divisao
+            //atualiza o tamanho do maior lado
             new_size = h / 2;
             count++;
             h = new_size;
