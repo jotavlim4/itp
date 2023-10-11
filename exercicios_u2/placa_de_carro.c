e37ff11ecd095b092304cb2987dffe45026cb31b
jotavlim4 <jotav.lim4@gmail.com>
2023-10-11 14:42:24 -0300

    Solução de exercício 3 - lista strings

--- exercicios_u2/placa_de_carro.c
+++ exercicios_u2/placa_de_carro.c
@@ -1,23 +1,8 @@
+
 #include <stdio.h>
 #include <ctype.h>
 
 #define TAM_MAX 21
-
-int str_len(char vet[]){
-    int i;
-    int count;
-    
-    i = 0;
-    count = 0;
-    while(vet[i] != '\n'){
-        if(vet[i] == '\n')
-            vet[i] = '\0';
-        else
-            count++;
-        i++;
-    }
-    return count;
-}
 
 int validate_plate(char vet[]){
     for(int i = 0; i < 3; i++){
@@ -34,21 +19,19 @@ int validate_plate(char vet[]){
     }
 
     return 1;
-
 }
 
 int main(){
     char plate[TAM_MAX];
     
-    fgets(plate, TAM_MAX, stdin); //fgets lê a string até encontrar o '\n'
-    str_len(plate);
-
-    //printf("%d\n", str_len(plate));
-    if(str_len(plate) != 8)
-        printf("não\n")
-    
-    if(validate_plate(plate))
+    scanf("%s", plate);
+        
+    if(strlen(plate) != 8)
+        printf("não\n");
+        
+    else if(validate_plate(plate))
         printf("sim\n");
+        
     else
         printf("não\n");
               
