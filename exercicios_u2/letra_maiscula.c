21213cf04198aef94f0fb565354882227a9b4248
João Victor <jotav.lim4@gmail.com>
2023-10-11 16:16:01 -0300

    Ajuste nos parâmetros das funções

--- exercicios_u2/letra_maiscula.c
+++ exercicios_u2/letra_maiscula.c
@@ -2,33 +2,40 @@
 #include <string.h>
 #include <ctype.h>
 
-
 #define TAM 61
 
-int string_lenght(int len, char string[len]){
+int string_lenght(char string[])
+{
   int count = 0;
-  
-  while(string[count] != '\n'){
-    if(string[count] != '\0')
+
+  while (string[count] != '\n')
+  {
+    if (string[count] != '\0')
       count++;
   }
   return count;
 }
 
-void toLowercase(int len, char string[len]){
+void toLowercase(char string[])
+{
   int i = 0;
-  while(string[i] != '\0'){
+  while (string[i] != '\0')
+  {
     string[i] = tolower(string[i]);
     i++;
   }
 }
 
-void first_upper(int len, char string[len]){
-  for(int i = 0; i < len; i++){
-    if(string[i - 1] == ' ' && i > 0){
+void first_upper( char string[])
+{
+  for (int i = 0; i < string_lenght(string); i++)
+  {
+    if (string[i - 1] == ' ' && i > 0)
+    {
       string[i] = toupper(string[i]);
     }
-    else if(i == 0){
+    else if (i == 0)
+    {
       string[i] = toupper(string[i]);
     }
     else
@@ -36,21 +43,21 @@ void first_upper(int len, char string[len]){
   }
 }
 
-int main(){
+int main()
+{
   char text[TAM];
   int lenght;
-  
-  fgets(text, TAM, stdin); //inclui o \n na string
 
-  //calcula tamanho da string
-  lenght = string_lenght(TAM, text);
-  //faz toda a string ser minuscula
-  toLowercase(lenght, text);
-  //apenas primeira letra maisucla
-  first_upper(lenght, text);
+  fgets(text, TAM, stdin); // inclui o \n na string
+
+  // calcula tamanho da string
+  lenght = string_lenght(text);
+  // faz toda a string ser minuscula
+  toLowercase(text);
+  // apenas primeira letra maisucla
+  first_upper(text);
 
   printf("%s", text);
-
 
   return 0;
 }
